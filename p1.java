0adf44dc8eb4611fc4732c2b74494b08a74708ee
hae young joo <jhy753@gmail.com>
2017-02-12 09:56:56 +0900

    문제 두 수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
    입력
    첫째 줄에 A와 B가 주어진다. (0 < A,B < 10)
    
    출력
    첫째 줄에 A+B를 출력한다.

---
+++ p1.java
@@ -0,0 +1,20 @@
+﻿import java.util.*;
+
+public class p1 {
+
+  
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+  
+  
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+  
+        int c = a+b;
+       
+        System.out.println(c);
+       
+    }
+
+}
