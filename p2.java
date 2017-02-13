7dc51d1f073616c96857c13c6899f9333acc2bc8
hae young joo <jhy753@gmail.com>
2017-02-13 10:30:39 +0900

    2~4번 문제

---
+++ p2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class p2 {
+
+  
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+  
+  
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+  
+        int c = a+b;
+       
+        System.out.println(c);
+       
+    }
+
+}
---
+++ p3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class p3 {
+
+  
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+		
+		int question = scan.nextInt();
+		
+		for(int i = 0; i<question ; i++){
+		
+		 int a = scan.nextInt();
+         int b = scan.nextInt();
+		 System.out.println(a+b);
+			
+		}
+		
+       
+    }
+
+}
