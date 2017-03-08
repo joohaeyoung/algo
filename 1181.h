09eb0396c15c6aaf6caed08b1a459724c3087f82
hae young joo <jhy753@gmail.com>
2017-03-08 14:17:08 +0900

    좌표정렬하기

--- 1181.h
+++ 1181.h
@@ -59,16 +59,10 @@ int main() {
 
 	cin >> n;
 
-
-
 	vector<string> a(n);
 
-
-
 	for (int i = 0; i<n; i++) {
-
 		cin >> a[i];
-
 	}
 
 	sort(a.begin(), a.end(), [](string u, string v) {
@@ -76,23 +70,18 @@ int main() {
 		if (u.size() == v.size()) {
 
 			return u < v;
-
 		}
 		else {
 
 			return u.size() < v.size();
-
 		}
-
 	});
-
+	
 	a.erase(unique(a.begin(), a.end()), a.end());
-
 	for (string s : a) {
 
 		cout << s << '\n';
 	}
-
 	return 0;
 
 }
